b763232fab6c9825a8fbb22a425f7f0256c7fe9c
Jack <jdempsey1993@gmail.com>
2019-07-18 13:24:37 -0400

    Pseudocode layout of javascript file.

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+// Show the user random integer number between 19-120
+
+    // Chose random integer value inbetween set parameters
+
+    // DOM-- documentget element by id display to div id "#randomNumber"
+
+
+// Assign the crystals random integer values
+
+    // Give each crystal #id a random value 1-12 ; hidden value
+
+
+
+// On click function for each image
+
+    // Add random value of crystal click to var userScore
+
+
+    
+// Function to win game 
+
+    // If userscore integer == random number score
+
+    // Add +1 to wins, documentgetelmentId to display number of wins
+
+
+
+//Function game loss
+
+    // If userscore integer > random number integer
+
+    //Add +1 to loss, documentgetelement to display in HTML
+
+
+
+// When number of losses or wins increases by one, game reset
+
+
+    // Set an new rando integer value from 19-120 to randomscore
+
+    // Assign random values 1-12 to each respective crystal id 
